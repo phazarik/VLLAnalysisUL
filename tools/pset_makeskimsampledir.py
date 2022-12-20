6fc00fad8aee098c03dfb4df3921a5262b43568b
Prachurjya <prachurjyahazarika@gmail.com>
2022-12-20 17:26:42 +0530

    Working on skimmed samples (2mu, basic)

--- tools/pset_makeskimsampledir.py
+++ tools/pset_makeskimsampledir.py
@@ -1,31 +1,29 @@
 import os
 
-dirlist=os.listdir()
-#print(dirlist)
+dumped_dir = "/home/work/phazarik1/work/VLLanalysis/SkimmedSamples/output"
+dirlist=os.listdir(dumped_dir)
 
-jobname="VLL2018_1L2JSkimmer_Aug4_v1_"
-bkggroup=["TTBar","WW","WZ","ZZ"]
+jobname="VLLAna_2muSkimmed_Dec19"
+#bkggroup=["HTbinnedWJets", "QCD_MuEnriched", "SingleTop", "TTBar", "WW", "WZ", "ZZ"]
+bkggroup=["DYJetsToLL"]
+print("")
 
-for bkg in bkggroup[1:]:
+for bkg in bkggroup:
     samplegroup=bkg
     for item in dirlist:
-        #print(item)
-        if(item.startswith(jobname+bkg)):
-            #samplegroup=item.split("VLL2018_1L2JSkimmer_Aug4_v1_")[1].split("sample")[0]
-            subsamplename=item.split("VLL2018_1L2JSkimmer_Aug4_v1_")[1].split("_sample")[0].split(bkg+"_")[1]
-            #print(samplegroup)
-            #print(subsamplename)
-            os.makedirs(samplegroup+"/"+subsamplename)
-            #fname="VLL2018_1L2JSkimmer_Aug4_v1"+"_"+samplegroup+"_"+subsamplename+"_sample"
-    
-            #if(item.startswith(fname)):
-            #print(item)
-            processline= "mv "+item+"/* "+samplegroup+"/"+subsamplename+"/"
-            
-            print(processline)
-
+        if(item.startswith(jobname+"_"+bkg)):
+            #print("moving ..."+item)
+            subsamplename=item.split(jobname+"_")[1].split("_sample")[0].split(bkg+"_")[1]
+            outdir = "Skimmed2018/"+jobname+"/"+bkg+"/"+subsamplename
+            #print(outdir)
+            files = "output/"+item+"/*"+subsamplename+"*.root"
+            processline= "mv "+files+" "+outdir+"/"
+            #moved_files = os.listdir(outdir)
+            #print(processline)
+            #print(files)
+            #example: mv VLLAna_2muSkimmed_Dec15_DYJetsToLL_M50_sample__121522_104436/* Skimmed2018/DYJetsToLL/M50/
+            os.makedirs(outdir)
             os.system(processline)
-            print("\ncopying complete.....\n")
-            print(samplegroup+"and"+subsamplename+"\n.........................")
-            
-            os.system("ls "+samplegroup+"/"+subsamplename+"/")
+    print(samplegroup+" files have been moved sucessfully.\n")
+
+print("\nAll files moved sucessfully!\n")
